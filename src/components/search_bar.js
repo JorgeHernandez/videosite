294e544b66bcc095bbd41705236f84c84b9a6c47
Jorge Hernández <diego.hernandez@i3television.es>
2017-06-11 16:16:51 +0200

    basic search bar

---
+++ src/components/search_bar.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+class SearchBar extends React.Component{
+	constructor(props){
+		super(props);
+		this.state = { term: ''};
+	}
+
+	render() {
+		//return <input onChange={this.onInputChange} />;
+		return (
+			<div>
+				<input 
+					value={this.state.term}
+					onChange={ (event)=>this.setState({term: event.target.value}) } 
+				/>
+				value of the input: {this.state.term};
+			</div>
+		);
+	}
+
+	onInputChange(event){
+		//console.log(event.target.value);
+		//this.setState({term: event.target.value});//error: this is undefined. moved to event handler
+	}
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
 
 const API_KEY = 'AIzaSyAFr4834h7QFUCwScY7HVxhBHCrYZIEfrk';
 
 
+
 const App = ()=>{
-	return <div>hi</div>
+	return (
+		<div>
+			<SearchBar />
+		</div>);
 }
 
 ReactDOM.render(<App />, document.querySelector('.container') );
