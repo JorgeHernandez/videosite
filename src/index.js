1809d2cba883c11fb36617b53a962dcbf8e91822
Jorge Hernández <diego.hernandez@i3television.es>
2017-06-11 16:27:39 +0200

    inital search

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
+import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyAFr4834h7QFUCwScY7HVxhBHCrYZIEfrk';
 
 
 
-const App = ()=>{
-	return (
-		<div>
-			<SearchBar />
-		</div>);
+class App extends Component{
+	constructor(props){
+		super(props);
+
+		this.state = { videos: [] };
+
+		YTSearch({key: API_KEY, term: 'cats'}, (data)=>{
+			this.setState({videos: data});//could be {videos} in ES6 if data is renamed as videos
+			console.log(data);
+		});
+
+	}
+
+	render(){
+		return (
+			<div>
+				<SearchBar />
+			</div>
+		);
+	}
 }
 
 ReactDOM.render(<App />, document.querySelector('.container') );
