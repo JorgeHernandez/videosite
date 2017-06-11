a99e48a7b5b6e9747a808bed2d9f4aab484596bc
Jorge Hernández <diego.hernandez@i3television.es>
2017-06-11 17:48:57 +0200

    thumbs and title added for each video

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
 const VideoListItem = (props)=>{
-	return <div>video</div>
+	const video = props.video;
+	const imageUrl = video.snippet.thumbnails.default.url;
+	return (<li className="list-group-item">
+		video
+		<div className="video-list media">
+			<div className="media-object">
+				<img className="media-object" src={imageUrl} />
+			</div>
+			<div className="media-body">
+				<div className="media-heading">{video.snippet.title}</div>
+			</div>
+		</div>
+		</li>
+	);
 }
 
 export default VideoListItem;
