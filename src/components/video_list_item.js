9bb5c7c6e4324642e307df28bb2e38695b20cd6a
Jorge Hernández <diego.hernandez@i3television.es>
2017-06-11 17:38:14 +0200

    basic fetch

---
+++ src/components/video_list_item.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const VideoListItem = (props)=>{
+	return <div>video</div>
+}
+
+export default VideoListItem;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import React from 'react';
+
+const VideoListItem = (props) =>{
+	return (<li>video</li>);
+};
+
+export default VideoListItem;
+*/
